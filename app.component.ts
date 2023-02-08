bd6adcaab1472ddb3aa615d64416b4a5f198309a
hannah-dubose <73614102+hannah-dubose@users.noreply.github.com>
2023-02-08 18:29:23 -0500

    Create app.component.ts

---
+++ app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <!-- header -->
+    <app-header></app-header>
+
+    <app-home></app-home>
+
+    <!-- routes get injected here -->
+    
+    <router-outlet></router-outlet>
+
+    <!-- footer -->
+
+    <app-footer></app-footer>
+
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'my-angular-site';
+}
