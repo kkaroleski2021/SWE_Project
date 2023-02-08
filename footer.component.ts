b1ff030d2525f03438ec60e05441ed52653ee571
hannah-dubose <73614102+hannah-dubose@users.noreply.github.com>
2023-02-08 18:30:46 -0500

    Create footer.component.ts

---
+++ footer.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  template: `
+
+     <footer class="footer">
+      <div class="container content has-text-centered">
+        <p> By: Hannah, Ava, Natasha, and Katja </p>
+    </div>
+  </footer>
+
+  `,
+  styles: [
+  ]
+})
+export class FooterComponent {
+
+}
