1874139172e72d3977fcb1b5afca032d02c33814
natashasthilaire <93110503+natashasthilaire@users.noreply.github.com>
2023-02-08 18:39:54 -0500

    Add files via upload
    Allows push, update, post, and delete requests

--- main.go
+++ main.go
@@ -1,52 +1,26 @@
 package main
 
-/*
-	setup: see github_cmds.md
-*/
-
 import (
+	"log"
+	"net/http"
 
-	//"time" //commented out rn .. line 45
-	//"database/sql"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
-
-	/*
-		each package is located in a respective folder in the src dir (where main.go is located)
-		each package is accessible from main.go by writing "go/<package name>"
-	*/
-	"go/entity"
-	"go/router"
+	"github.com/gorilla/mux"
 )
 
-var (
-	search entity.Search
-)
+func initializeRouter() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/users", GetUsers).Methods("GET")
+	r.HandleFunc("/users/{id}", GetUser).Methods("GET")
+	r.HandleFunc("/users", CreateUser).Methods("POST")
+	r.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
+	r.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":9000", r))
+
+}
 
 func main() {
-	server := gin.Default()
-	router.NewRouter(&router.Config{
-		R: server,
-	})
-
-	/*demo templates for sprint 1*/
-	server.Static("/css", "./S1_DEMO_templates/css")
-	server.LoadHTMLGlob("S1_DEMO_templates/*.html")
-
-	/*//sql stuff in progress
-		db.Ping() checks if user, pass is valid
-		pw may or may not be sensitive? leaving out for now
-		db, err := sql.Open("mysql", "root:PASSWORD@tcp(127.0.0.1:3306)/storeDB")
-		if err != nil {
-	 	panic(err) //do proper error handling
-		}
-		db.SetConnMaxLifetime(time.Minute * 3) //ensure connections are closed by driver safely before connection is closed by MySQL server, OS, or other middlewares
-		db.SetMaxOpenConns(10)
-		db.SetMaxIdleConns(10)
-	*/
-
-	//may change to 4200 when adding angular elements
-	server.Run(":8080")
-	//defer db.Close()
+	InitialMigration()
+	initializeRouter()
 }
