107da64515ef3511c18d17effeb40dbec0d1bf9d
Katja <92826991+kkaroleski2021@users.noreply.github.com>
2023-02-08 22:02:41 -0500

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import { ProfilesComponent } from './profiles/profiles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
     ProfilesComponent
   ],
   imports: [
