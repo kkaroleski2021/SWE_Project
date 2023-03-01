66514e256fdb4b6cad131c695cade9a9eb51f368
hannah-dubose <73614102+hannah-dubose@users.noreply.github.com>
2023-02-28 23:19:39 -0500

    Update footer.component.ts

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   template: `
+
      <footer class="footer">
       <div class="container content has-text-centered">
         <p> By: Hannah, Ava, Natasha, and Katja </p>
     </div>
   </footer>
+
   `,
   styles: [
   ]
