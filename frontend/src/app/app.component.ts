87dc34e15bab0df7b41a68695dc83ea99c96fd0c
hannah-dubose <73614102+hannah-dubose@users.noreply.github.com>
2023-02-28 23:16:55 -0500

    Update app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,3 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'the software engineering project of all time';
-// }
-
 import { Component } from '@angular/core';
 
 @Component({
@@ -16,10 +5,8 @@ import { Component } from '@angular/core';
   template: `
     <!-- header -->
     <app-header></app-header>
-    <app-home></app-home>
+    <a-component></a-component>
     
-    <app-profiles></app-profiles>
-
     <!-- routes get injected here -->
     
     <router-outlet></router-outlet>
