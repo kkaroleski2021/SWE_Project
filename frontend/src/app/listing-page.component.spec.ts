521ee0e1c4dab63a1eff7f5b8d3a5ee49e7e2cc3
Katja <92826991+kkaroleski2021@users.noreply.github.com>
2023-03-01 20:03:12 -0500

    Add files via upload

---
+++ frontend/src/app/listing-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListingPageComponent } from './listing-page.component';
+
+describe('ListingPageComponent', () => {
+  let component: ListingPageComponent;
+  let fixture: ComponentFixture<ListingPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListingPageComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ListingPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
