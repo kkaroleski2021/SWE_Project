79ad2163d83b87dce48c8c4dfd726db3e33cc133
Natasha St Hilaire <natashasthilaire@ufl.edu>
2023-04-17 17:33:37 -0400

    entityModel

---
+++ product/tableOrderedProducts.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS orderedProduct;
+
+CREATE TABLE orderedProduct (
+  id          VARCHAR(256) NOT NULL,
+  product_id  VARCHAR(256) NOT NULL,
+  quantity    INT NOT NULL,
+  order_id    VARCHAR(256) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+ALTER TABLE orderedProduct CONVERT TO CHARACTER SET utf8;
+
+INSERT INTO orderedProduct
+  (id, product_id, quantity,  order_id)
+VALUES
+  ('2fb805ed-99b3-43d9-6752-9b2b15f9d9d7', 'bc264186-9c2e-4533-6ba5-705c160303c1', 1, 'a980ced5-4acb-4be8-51e3-4565e06082ea'),
+  ('dc841f8a-91c4-45d1-6ead-d278bf3b48ec', '376d4186-4533-9c2e-6ba5-484c1604543c', 1, 'a980ced5-4acb-4be8-51e3-4565e06082ea'),
+  ('7d03c974-0dfe-462e-7f13-5a1e5fc93bdb', '0b45c13b-376d-4a1c-6941-4a7bd9ba03fc', 1, 'a980ced5-4acb-4be8-51e3-4565e06082ea'),
+  ('c3eb71b9-041a-44b4-532c-0a0268157a5c', '1ac5fc58-8360-48a1-526a-6dfbf8c3fb59', 3, 'b8aeed64-1c6d-40bb-6d5e-6e1efbab5dcb');
