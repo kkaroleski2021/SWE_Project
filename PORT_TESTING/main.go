ccee11f39e53b3503566e06f916e0de15f673b37
Avary McCormack <64299012+avary8@users.noreply.github.com>
2023-04-19 12:45:33 -0400

    need CORS header i think

--- PORT_TESTING/main.go
+++ PORT_TESTING/main.go
@@ -11,6 +11,7 @@ import (
 	//"github.com/TutorialEdge/realtime-chat-go-react/pkg/websocket"
 	"github.com/gorilla/mux"
 	//"github.com/gorilla/websocket"
+	"github.com/rs/cors"
 )
 
 func getOrigin() *url.URL {
@@ -23,11 +24,16 @@ var origin = getOrigin()
 var director = func(req *http.Request) {
 	req.Header.Add("X-Forwarded-Host", req.Host)
 	req.Header.Add("X-Origin-Host", origin.Host)
+	req.Header.Set("Access-Control-Allow-Origin", "*")
 	req.URL.Scheme = "http"
 	req.URL.Host = origin.Host
 }
 
 var AngularHandler = &httputil.ReverseProxy{Director: director}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
 
 func httpHandler() {
 	r := mux.NewRouter()
@@ -39,8 +45,25 @@ func httpHandler() {
 	r.HandleFunc("/page3", basic_functions.Page3).Methods("GET")
 
 	r.PathPrefix("/").Handler(AngularHandler).Methods("GET")
+	//cors optionsGoes Below
+	corsOpts := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:9000"}, //you service is available and allowed for this base url
+		AllowedMethods: []string{
+			http.MethodGet, //http methods for your app
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodPatch,
+			http.MethodDelete,
+			http.MethodOptions,
+			http.MethodHead,
+		},
 
-	log.Fatal(http.ListenAndServe(":9000", r))
+		AllowedHeaders: []string{
+			"Access-Control-Allow-Origin",
+		},
+	})
+
+	log.Fatal(http.ListenAndServe(":9000", corsOpts.Handler(r)))
 }
 
 func main() {
