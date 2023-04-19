50e9d34fa1b97046e09bca3edf92629b49f5a5db
hannah-dubose <73614102+hannah-dubose@users.noreply.github.com>
2023-04-19 17:00:48 -0400

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CComponent} from './login/login.component';
+import {AComponent} from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'c-component',
+    component: CComponent
+  },
+  {
+    path: '',
+    component: AComponent
+  },
+  {
+    path: "signup",
+    component: SignupComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
