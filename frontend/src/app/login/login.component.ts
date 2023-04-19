a0f7ffb5e878ea077ae9451b47e8fda19c09e65c
hannah-dubose <73614102+hannah-dubose@users.noreply.github.com>
2023-04-19 16:57:41 -0400

    Create login.component.ts

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'c-component',
+  template: `
+    <form class="box">
+  <div class="field">
+    <label class="label">Email</label>
+    <div class="control">
+      <input class="input" type="email" placeholder="e.g. alex@example.com">
+    </div>
+  </div>
+
+  <div class="field">
+    <label class="label">Password</label>
+    <div class="control">
+      <input class="input" type="password" placeholder="********">
+    </div>
+  </div>
+
+<button is-link onclick="document.location='//localhost:62554/'">Log In</button>
+
+
+  <p class="control">
+  <button class is-link onclick="document.location='//localhost:62845/'">
+      Don't have an account? Sign up now!
+    </button>
+  </p>
+
+</form>
+
+  `,
+  styles: [`
+     `]
+})
+
+export class CComponent {
+
+}
