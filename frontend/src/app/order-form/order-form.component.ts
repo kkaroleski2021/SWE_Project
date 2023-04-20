3ebbbb6e55a0cf8afe51588e13789a92b4cd56a1
Katja <92826991+kkaroleski2021@users.noreply.github.com>
2023-04-19 20:47:32 -0400

    Add files via upload

---
+++ frontend/src/app/order-form/order-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-order-form',
+  templateUrl: './order-form.component.html',
+  styleUrls: ['./order-form.component.css']
+})
+export class OrderFormComponent {
+
+}
