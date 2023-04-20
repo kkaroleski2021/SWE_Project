323aac7aa5b7ae4ccf47fd868750d88b7e0773f3
Katja <92826991+kkaroleski2021@users.noreply.github.com>
2023-04-19 22:53:42 -0400

    Update order-form.component.ts

--- frontend/src/app/order-form/order-form.component.ts
+++ frontend/src/app/order-form/order-form.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-order-form',
-  templateUrl: './order-form.component.html',
-  styleUrls: ['./order-form.component.css']
-})
-export class OrderFormComponent {
+  template: `
+    <form #f="ngForm" (ngSubmit)="onSubmit(f)" novalidate>
+      <input name="first" ngModel required #first="ngModel">
+      <input name="last" ngModel>
+      <button>Submit</button>
+    </form>
 
+    <p>First name value: {{ first.value }}</p>
+    <p>First name valid: {{ first.valid }}</p>
+    <p>Form value: {{ f.value | json }}</p>
+    <p>Form valid: {{ f.valid }}</p>
+  `,
+  templateUrl: './order-form.component.html',
+  styleUrls: ['./order-form.component.css'],
+  
+})
+export class OrderFormComponent implements OnInit {
+    constructor() {
+      document.title = 'OrderForm';
+    }
+
+    ngOnInit(): void {
+      
+    }
+
+    onSubmit(f: NgForm) {
+      console.log(f.value);  // { first: '', last: '' }
+      console.log(f.valid);  // false
+    }
 }
