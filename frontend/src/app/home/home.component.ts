215a69518925185f05fc9e7bd61d2342f8def494
Katja <92826991+kkaroleski2021@users.noreply.github.com>
2023-04-19 22:53:07 -0400

    Update home.component.ts

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,23 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'a-component',
+  selector: 'app-home',
   template: `
     <section class="hero is-primary is-bold is-fullheight">
   <div class="hero-body">
-      <b-component></b-component>
+  
     
   </div>
+<app-profiles></app-profiles>
 </section>
   `,
   styles: [`
      .hero  {
-        background-image: url('/assets/img/background2.jpg') !important;
+        background-image: url('/assets/images/background2.jpg') !important;
         background-size: cover;
         background-position: center center;
      }
      `]
+
 })
 
-export class AComponent {}
+export class HomeComponent {
 
+}
