3db0c1cca67d2746ed03ad007d424fae28233299
Natasha St Hilaire <natashasthilaire@ufl.edu>
2023-04-19 20:45:25 -0400

    test delete user profile:

--- user/util/util_test.go
+++ user/util/util_test.go
@@ -323,3 +323,25 @@ func Test_UploadImage(t *testing.T) {
 			rr.Body.String(), expected)
 	}
 }
+
+func TestDeleteEntry(t *testing.T) {
+	req, err := http.NewRequest("DELETE", "/entry", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	q := req.URL.Query()
+	q.Add("id", "4")
+	req.URL.RawQuery = q.Encode()
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(user.DeleteUser)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	expected := `{"id":4,"first_name":"xyz change","last_name":"pqr","email_address":"xyz@pqr.com","phone_number":"1234567890"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
