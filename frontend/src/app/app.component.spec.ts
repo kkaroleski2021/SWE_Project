60770535d11a239293fc8fed409a320025a06960
hannah-dubose <73614102+hannah-dubose@users.noreply.github.com>
2023-04-19 22:39:42 -0400

    Update app.component.spec.ts

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,35 +1,74 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ListingPageComponent } from './listing-page/listing-page.component';
+import { BComponent} from './login/login.component';
+import { SComponent } from './signup/signup.component';
+import { Header2Component } from './loginHeader/loginHeader.component'; 
+
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+beforeEach(async () => {
+await TestBed.configureTestingModule({
+declarations: [
+AppComponent
+],
+}).compileComponents();
+});
+
+it(`should have as title 'SimpleApp'`, () => {
+const fixture = TestBed.createComponent(AppComponent);
+const app = fixture.componentInstance;
+expect(app.title).equal('SimpleApp');
+});
+
+it('should render title', () => {
+const fixture = TestBed.createComponent(AppComponent);
+fixture.detectChanges();
+const compiled = fixture.nativeElement as HTMLElement;
+expect(compiled.querySelector('.content span')?.textContent).contain('SimpleApp app is running!');
+
+});
+
+beforeEach(() => {
+  TestBed.configureTestingModule({
+    imports: [
+        AppRoutingModule.withRoutes(
+        [{path: '', component: HomeComponent}, {path: 'Login', component: BComponent}, {path: 'signup', component: SComponent}]
+      )
+    ]
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+})
+
+it('should render title', () => { //6
+    const fixture = TestBed.createComponent(Header2Component);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).contain('Log In to Swampy Sells!');
   });
 
   it(`should have as title 'hello-world'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hello-world');
+    expect(app.title).equal('hello-world');
   });
 
-  it('should render title', () => {
+  it('should set image path as expected', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hello-world app is running!');
-  });
+    const ele = fixture.debugElement.nativeElement.querySelectorAll('img');
+    expect(ele[1]['src']).contain('assets/images/stuffedAnimalChair.jpg');
+    expect(ele[2]['src']).contain('assets/images/clamshellVase.jpg');
+    expect(ele[3]['src']).contain('assets/images/grandmasCouch.jpg');
+    expect(ele[4]['src']).contain('assets/images/clamshellVase.jpg');
 });
+
+});
+
+
+
